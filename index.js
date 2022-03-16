dcc173425733edeba31bfc102cb814f2bb25cd91
Alexandra Davis <alex.davis806@gmail.com>
2022-03-15 19:37:50 -0700

    commit tree d878572c4d87382b301191dc2c15d070ff435045

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+// saturdayFun("bathe my dog");
+// console.log(saturdayFun());
+// console.log(saturdayFun("bathe my dog"))
+
+
+function mondayWork(todo = "go to the office") {
+    return `This Monday, I will ${todo}.`;
+}
+
+// console.log(mondayWork());
+
+
+function wrapAdjective(param = "*") {
+    return function(single = "special") {
+        return `You are ${param}${single}${param}!`
+    }
+}
+
+console.log(wrapAdjective('*')('a hard worker'))
+console.log(wrapAdjective('||')('a dedicated programmer'));
